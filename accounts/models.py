82f74b3791c164a6c2ce77b7f465982d871ca84b
Ivan Ivanov <ivan2.ivanov91@gmail.com>
2025-01-30 22:15:20 +0100

    feat: first model for user and profile created as well as requirements.txt file adjusted

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User 
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = ["email"]
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customer_user_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',
+        blank=True
+
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True)
+   # avatar = models.ImageField(upload_to='avatars/', blank=True, null=True) need to add Pillow model
+    location = models.CharField(max_length=100, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
