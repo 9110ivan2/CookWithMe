b102ab696655b37223dd6d9fdb8ed437a51273ef
Ivan Ivanov <ivan2.ivanov91@gmail.com>
2025-05-09 12:03:51 +0300

    feat: added login and logout classes to the accounts app, created a permission file and a class for to write and for all other users to readonly

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from rest_framework import generics
 from accounts.models import Profile, User
 from accounts.serializers import UserSerializer, ProfileSerializer
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 
@@ -20,10 +24,31 @@ class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
 
 class RegisterView(APIView):
+    permission_classes = []  # Allow any user to register
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-    permission_classes = []  # Allow any user to register
+
+class Login(APIView):
+    permission_classes = []  # Allow any user to login
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"error": "Invalid credentials"},
+                status=status.HTTP_401_UNAUTHORIZED)
+        
+class Logout(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request, *args, **kwargs):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
