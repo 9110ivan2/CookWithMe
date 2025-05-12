f6c22a6ea6ed635b53d89e0d167b2edede953d5d
Ivan Ivanov <ivan2.ivanov91@gmail.com>
2025-05-12 16:36:13 +0300

    feat: added a function post to CategoryListCreateView only for superusers to be able create categories, added permissions to the CategoryDetailView only for adminusers to be able to make changes to categories, added permission to RecipeListCreateView that allows auth users to write and read whereas unauth users only to read recipes

--- categories/views.py
+++ categories/views.py
@@ -1,17 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from categories.models import Category, RecipeCategory
 from categories.serializers import Categoryserializer, RecipeCategoryserializer
-
+from rest_framework.response import Response
+from rest_framework import status, filters
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = Categoryserializer
 
+    def post(self, request, *args, **kwargs):
+        if not request.superuser.is_authenticated:
+            return Response({"error": "You must be logged in to create a category."}, status=status.HTTP_403_FORBIDDEN)
+        return self.create(request, *args, **kwargs)
+
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAdminUser]
     queryset = Category.objects.all()
     serializer_class = Categoryserializer
 
 class RecipeListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = RecipeCategory.objects.all()
     serializer_class = RecipeCategoryserializer
 
