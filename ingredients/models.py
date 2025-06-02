743daee181120114e01997a863df4e640a95071b
Ivan Ivanov <ivan2.ivanov91@gmail.com>
2025-06-02 17:13:39 +0300

    refactor: altered ingredient model field description not to have unique description, altered RecipeIngredient fields quantity and unit to accept null or blank

--- ingredients/models.py
+++ ingredients/models.py
@@ -4,7 +4,7 @@ from recipes.models import Recipe
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True) 
-    description = models.TextField(blank=True, unique=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name 
@@ -13,8 +13,8 @@ class Ingredient(models.Model):
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredient')
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient')
-    quantity = models.DecimalField(max_digits=6, decimal_places=2)
-    unit = models.CharField(max_length=50)
+    quantity = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    unit = models.CharField(max_length=50,blank=True, null=True)
 
     class Meta:
         unique_together=('recipe','ingredient') #TODO what is this for 
