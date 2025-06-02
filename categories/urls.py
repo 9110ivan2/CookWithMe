d47856e5b5d5a4eab7e53ca00f9828e724b6ae7d
Ivan Ivanov <ivan2.ivanov91@gmail.com>
2025-06-02 15:08:50 +0300

    fix: Recipecategory views and ulrs removed

--- categories/urls.py
+++ categories/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from categories.views import RecipeListCreateView, RecipeDetailView, CategoryListCreateView, CategoryDetailView
-
+from categories.views import CategoryListCreateView, CategoryDetailView 
 urlpatterns = [
-    path("recipe-list-create", RecipeListCreateView.as_view(), name="recipe-list-create"),
-    path("recipe-detail/<int:pk>/", RecipeDetailView.as_view(), name="recipe-detail"),
-    path("category-list-create", CategoryListCreateView.as_view(), name="category-list-create"),
+    path("category-list-create/", CategoryListCreateView.as_view(), name="category-list-create"),
     path("category-detail/<int:pk>/", CategoryDetailView.as_view(), name="category-detail"),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,16 +1,22 @@
 from rest_framework import generics, permissions
-from categories.models import Category, RecipeCategory
-from categories.serializers import Categoryserializer, RecipeCategoryserializer
+from categories.models import Category
+from categories.serializers import Categoryserializer
 from rest_framework.response import Response
 from rest_framework import status, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import  AllowAny
+
 # Create your views here.
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = Categoryserializer
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        if not request.superuser.is_authenticated:
+        print("DEBUG: request.user =", request.user) # TODO remove in production
+        print("DEBUG: is_authenticated =", request.user.is_authenticated) # TODO remove in production
+        print("DEBUG: is_superuser =", request.user.is_superuser) # TODO remove in production
+        if not request.user.is_authenticated or not request.user.is_superuser:
             return Response({"error": "You must be logged in to create a category."}, status=status.HTTP_403_FORBIDDEN)
         return self.create(request, *args, **kwargs)
 
@@ -18,12 +24,3 @@ class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAdminUser]
     queryset = Category.objects.all()
     serializer_class = Categoryserializer
-
-class RecipeListCreateView(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = RecipeCategory.objects.all()
-    serializer_class = RecipeCategoryserializer
-
-class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = RecipeCategory.objects.all()
-    serializer_class = RecipeCategoryserializer
