4772d871f058e7d980cc757b1197714522e266a3
Ivan Ivanov <ivan2.ivanov91@gmail.com>
2025-06-03 18:32:43 +0300

    fix: nested urls for recipe app created to include the category of the recipe. views have been changed using the Mixin and inheritance, customized to use the category_id in the path

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from recipes.views import RecipeListCreateView, RecipeDetailView
 
 urlpatterns = [
-    path("recipes/", RecipeListCreateView.as_view(), name="recipe-list-create"),
-    path("recipe-detail/<int:pk>/", RecipeDetailView.as_view(), name="recipe-detail"),
-    #/recipes/?title=chicken
-    #/recipes/?author=JohnDoe
-    #/recipes/?title=chicken&author=JohnDoe to filter recipes by title and author
-
+    path("category-detail/recipes/",RecipeListCreateView.as_view(), name="recipe-list-create"),
+    path("category-detail/<int:category_id>/recipes/", RecipeListCreateView.as_view(), name="by-id-recipe-list-create"),
+    path("category-detail/<int:category_id>/recipe-detail/<int:pk>/", RecipeDetailView.as_view(), name="recipe-detail"),
+    # /recipes/?title=chicken
+    # /recipes/?author=JohnDoe
+    # /recipes/?title=chicken&author=JohnDoe to filter recipes by title and author
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,17 +3,40 @@ from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 from recipes.filters import RecipeFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+class CategoryRecipeQuerysetMixin:
+    """
+    Mixin to filter recipes by category_id if present in URL kwargs or query params.
+    """
+    def get_queryset(self):
+        # Try to get category_id from URL kwargs first, then from query params
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            return Recipe.objects.filter(categories__id=category_id)
+        return Recipe.objects.all()
 
-class RecipeListCreateView(generics.ListCreateAPIView):
-    queryset = Recipe.objects.all()
+    def perform_create(self, serializer):
+        category_id = self.kwargs.get('category_id') or self.request.data.get('category_id')
+        recipe = serializer.save(author=self.request.user)
+        if category_id:
+            recipe.categories.add(category_id)
+
+class RecipeListCreateView(CategoryRecipeQuerysetMixin, generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = RecipeSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
 
-
-class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.all()
+class RecipeDetailView(CategoryRecipeQuerysetMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RecipeSerializer
 
-# Create your views here.
+    def delete(self, request, *args, **kwargs):
+        category_id = self.kwargs.get('category_id')
+        self.category_id = category_id
+        return super().delete(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        category_id = self.kwargs.get('category_id')
+        self.category_id = category_id
+        return super().put(request, *args, **kwargs)
